a426ea6a0867317404e06a599dc2e67b3fe89e24
dann toliver <dann@bentobox.net>
2015-03-22 00:34:24 -0400

    some simple logging

--- one.js
+++ one.js
@@ -1,7 +1,11 @@
+function f_one(str) {
+  return ntrprt(parse(str))
+}
+
 function ntrprt(program) {
   return program.reduce(function(stack, word) {
-    if(+word == word) return stack.concat(word)        // numbers go on the stack
-    stack.push(eval(stack.pop() + word + stack.pop())) // ops are all binary
+    if(+word == word) return stack.concat(word)           // numbers go on the stack
+    stack.push(eval(stack.pop() + word + stack.pop()))    // ops are all binary
     return stack
   }, [])
 }
@@ -10,8 +14,7 @@ function parse(str) {
   return str.split(/\s+/)
 }
 
-function f(str) {
-  return ntrprt(parse(str))
-}
-
-// f('1.12332 2 3 + 4 7 % ^ *')
+console.log('f_one("2 3 +")', f_one("2 3 +"))             // addition is fine
+console.log('f_one("5 2 -")', f_one("5 2 -"))             // whoops this is backwards
+console.log('f_one("1.23 3 +")', f_one("1.23 3 +"))       // decimals are fine
+console.log('f_one("1 2 + 3 *")', f_one("1 2 + 3 *"))     // interleaved operators are fine
