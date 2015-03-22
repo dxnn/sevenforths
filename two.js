ae4b3c8c50365d7fa7b05095598ac593515aa7a8
dann toliver <dann@bentobox.net>
2015-03-22 00:10:16 -0400

    fancier code structure and logging

--- two.js
+++ two.js
@@ -1,23 +1,27 @@
-function ntrprt(program) {
-  var ops = ['+', '*', '-', '/', '%', '^', '|', '&', '||', '&&', '<', '>', '<<', '>>']
-  var dup = function(stack) {return stack.concat(stack[stack.length-1])}
-  
-  return program.reduce(function(stack, word) {
-    if(+word == word) return stack.concat(word)                  // numbers go on the stack
-    if(~ops.indexOf(word))
-      stack.push(eval(stack.pop() + word + stack.pop()))         // ops are all binary
-    else 
-      stack = eval(word+'('+JSON.stringify(stack)+')')           // funs are binary too
-    return stack
-  }, [])
-}
-
-function parse(str) {
-  return str.split(/\s+/)
-}
-
-function f(str) {
+function f_two(str) {
   return ntrprt(parse(str))
+
+  function ntrprt(program) {
+    var ops = ['+', '*', '-', '/', '%', '^', '|', '&', '||', '&&', '<', '>', '<<', '>>']
+    var dup = function(stack) {return stack.concat(stack[stack.length-1])}
+  
+    return program.reduce(function(stack, word) {
+      if(+word == word) return stack.concat(word)                  // numbers go on the stack
+      if(~ops.indexOf(word))
+        stack.push(eval(stack.pop() + word + stack.pop()))         // ops are all binary
+      else 
+        stack = eval(word+'('+JSON.stringify(stack)+')')           // funs are binary too
+      return stack
+    }, [])
+  }
+
+  function parse(str) {
+    return str.split(/\s+/)
+  }
 }
 
-// f('1.12332 2 3 + 4 7 % ^ *')
+
+var str, name = 'f_two'
+console.log(i = name+'("2 3 +")', eval(i))
+console.log(i = name+'f_one("2 3 + 4 7 % ^")', eval(i))
+console.log('f_one("1.12332 2 3 + 4 7 % ^ *")', eval(i))
