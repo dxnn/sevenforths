02ba002f2ca9e9fbd6375a0614319eb6a6a71da5
dann toliver <dann@bentobox.net>
2015-03-22 01:17:33 -0400

    more funs; flipped op params; more logging

--- two.js
+++ two.js
@@ -2,15 +2,19 @@ function f_two(str) {
   return ntrprt(parse(str))
 
   function ntrprt(program) {
-    var ops = ['+', '*', '-', '/', '%', '^', '|', '&', '||', '&&', '<', '>', '<<', '>>']
-    var dup = function(stack) {return stack.concat(stack[stack.length-1])}
+    var ops  = ['+', '*', '-', '/', '%', '^', '|', '&', '||', '&&', '<', '>', '<<', '>>']
+    var doop = function(stack, word) {var top=stack.pop(); return stack.pop() + word + top}
+    
+    var dup  = function(stack) {return stack.concat(stack[stack.length-1])}
+    var drop = function(stack) {stack.pop(); return stack}
+    var swap = function(stack) {stack.push(stack.pop(), stack.pop()); return stack}
   
     return program.reduce(function(stack, word) {
-      if(+word == word) return stack.concat(word)                  // numbers go on the stack
+      if(+word == word) return stack.concat(word)                   // numbers go on the stack
       if(~ops.indexOf(word))
-        stack.push(eval(stack.pop() + word + stack.pop()))         // ops are all binary
+        stack.push(eval(doop(stack, word)))                         // flip orientation of ops
       else 
-        stack = eval(word+'('+JSON.stringify(stack)+')')           // funs are binary too
+        stack = eval(word+'('+JSON.stringify(stack)+')')            // funs can be any arity
       return stack
     }, [])
   }
@@ -22,6 +26,11 @@ function f_two(str) {
 
 
 var str, name = 'f_two'
-console.log(i = name+'("2 3 +")', eval(i))
-console.log(i = name+'("5 2 -")', eval(i))
-console.log(i = name+'("1.23 2 3 + 4 7 % ^ *")', eval(i))
+console.log(i = name+'("2 3 +")', eval(i))                          // addition is still fine
+console.log(i = name+'("5 2 -")', eval(i))                          // subtraction is fine now too
+console.log(i = name+'("1.23 3 +")', eval(i))                       // decimals are fine
+console.log(i = name+'("1 2 + 3 *")', eval(i))                      // interleaved operators are still fine
+
+console.log(i = name+'("3 dup dup * *")', eval(i))                  // keywords work now
+console.log(i = name+'("3 4 5 drop drop dup dup * *")', eval(i))    // dropin' and dupin'
+console.log(i = name+'("2 5 swap -")', eval(i))                     // hot swap
