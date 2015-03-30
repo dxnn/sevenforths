8ffeab3b85ce0504cb48bed095c13ea2802d529c
dann toliver <dann@bentobox.net>
2015-03-30 11:04:04 -0400

    new words!

--- three.js
+++ three.js
@@ -4,22 +4,42 @@
 function f_three(str) {
   return ntrprt(parse(str))
 
-  function ntrprt(program) {
-    var ops  = ['+', '*', '-', '/', '%', '^', '|', '&', '||', '&&', '<', '>', '<<', '>>']
-    var doop = function(stack, word) {var top=stack.pop(); return stack.pop() + word + top}
+  function ntrprt(program, stack, dict) {
+    stack = stack || []
     
-    var dup  = function(stack) {return stack.concat(stack[stack.length-1])}
-    var drop = function(stack) {stack.pop(); return stack}
-    var swap = function(stack) {stack.push(stack.pop(), stack.pop()); return stack}
-  
-    return program.reduce(function(stack, word) {
-      if(+word == word) return stack.concat(word)                   // numbers go on the stack
-      if(~ops.indexOf(word))
-        stack.push(eval(doop(stack, word)))                         // flip orientation of ops
+    if(!dict) {      
+      dict = {}
+      dict['dup']  = function(stack) {stack.push(stack[stack.length-1]); return stack}
+      dict['drop'] = function(stack) {stack.pop(); return stack}
+      dict['swap'] = function(stack) {stack.push(stack.pop(), stack.pop()); return stack}
+
+      var ops  = ['+', '*', '-', '/', '%', '^', '|', '&', '||', '&&', '<', '>', '<<', '>>']
+      ops.forEach(function(op) {
+        dict[op] = function(stack) {
+          var top=stack.pop(); stack.push(eval(stack.pop() + op + top)); return stack } })
+    }
+    
+    
+    // function addword(dict, program, pc) {
+    
+    var pc = 0, max = program.length
+    while(pc < max) {
+      var word = program[pc]
+      if(word == ':') {
+        var stop = program.indexOf(';', pc)
+        var name = program[pc+1]
+        var sub = program.slice(pc+2, stop)
+        dict[name] = function(stack) {return ntrprt(sub, stack, dict)}
+        pc = stop
+      }
+        // addword(dict, program, pc)                     // use destructuring
+      else if(+word == word) 
+        stack.push(word)                                  // numbers go on the stack
       else 
-        stack = eval(word+'('+JSON.stringify(stack)+')')            // funs can be any arity
-      return stack
-    }, [])
+        stack = dict[word](stack)                         // funs can be any arity
+      pc++
+    }
+    return stack
   }
 
   function parse(str) {
@@ -28,19 +48,21 @@ function f_three(str) {
 }
 
 
-var str, name = 'f_three'
-var log = console.log.bind(console)
-var error = console.error.bind(console)
 var test = function(str, res) {
-  var fun = name+'("'+str+'")'
-  var out = eval(fun)
-  var say = JSON.stringify(out) == JSON.stringify(res) ? log : error
+  var error = console.error.bind(console)
+  var log   = console.log.bind(console)
+  var name = 'f_three'
+  var fun  = name+'("'+str+'")'
+  var out  = eval(fun)
+  var say  = JSON.stringify(out) == JSON.stringify(res) ? log : error
   say(fun, out)
 }
 
-test("2 3 +", [5])                                                  // addition is still fine
-test("5 2 -", [3])                                                  // subtraction is fine now too
-test("1.23 3 +", [4.23])                                            // decimals are fine
-test("1 2 + 3 *", [9])                                              // interleaved operators are still fine
-test("3 4 drop dup *", [9])                                         // keywords are still fine
-test("2 5 swap -", [3])                                             // hot swap
+test("2 3 +", [5])                                        // addition is still fine
+test("5 2 -", [3])                                        // subtraction is fine now too
+test("1.23 3 +", [4.23])                                  // decimals are fine
+test("1 2 + 3 *", [9])                                    // interleaved operators are still fine
+test("3 4 drop dup *", [9])                               // keywords are still fine
+test("2 5 swap -", [3])                                   // hot swap
+
+test("3 : dd dup dup ; dd * *", [27])                                   // hot swap
